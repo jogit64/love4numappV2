fc1cdbcf1b544d2ed0801006ac23682000e74ca3
jogit64 <usersfdc18@gmail.com>
2024-03-20 22:50:54 +0100

    V 1.0 versionCode 2 pub ID de PROD

--- adConfig.js
+++ adConfig.js
@@ -1,11 +1,11 @@
 // adConfig.js
 export const adMobConfig = {
-  infoBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - les 2 banner de infoscreen
-  //infoBannerId: "ca-app-pub-6996110830232375/1483175230",
-  loveBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - la banner sur love4numscreen le tirage
-  //loveBannerId: "ca-app-pub-6996110830232375/9643987853",
-  interstitialId: "ca-app-pub-3940256099942544/4411468910", // TEST l'intersticiel sur love4numscreen le tirage
-  //interstitialId: "ca-app-pub-6996110830232375/1579378159",
-  creditBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - la banner large sur creditscreen
-  //creditBannerId: "ca-app-pub-6996110830232375/9976742927",
+  // infoBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - les 2 banner de infoscreen
+  infoBannerId: "ca-app-pub-6996110830232375/1483175230",
+  //loveBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - la banner sur love4numscreen le tirage
+  loveBannerId: "ca-app-pub-6996110830232375/9643987853",
+  //interstitialId: "ca-app-pub-3940256099942544/4411468910", // TEST l'intersticiel sur love4numscreen le tirage
+  interstitialId: "ca-app-pub-6996110830232375/1579378159",
+  //creditBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - la banner large sur creditscreen
+  creditBannerId: "ca-app-pub-6996110830232375/9976742927",
 };
