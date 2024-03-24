0dc72bbdb8a51b8b1b207037fcda2067ec1c8c27
jogit64 <usersfdc18@gmail.com>
2024-03-24 10:34:19 +0100

    versionCode3 test active - aménagement du fichier adConfig pour switcher entre les version TEST/PROD - actuellement sur TEST

--- adConfig.js
+++ adConfig.js
@@ -1,11 +1,28 @@
 // adConfig.js
+
 export const adMobConfig = {
-  // infoBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - les 2 banner de infoscreen
-  infoBannerId: "ca-app-pub-6996110830232375/1483175230",
-  //loveBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - la banner sur love4numscreen le tirage
-  loveBannerId: "ca-app-pub-6996110830232375/9643987853",
-  //interstitialId: "ca-app-pub-3940256099942544/4411468910", // TEST l'intersticiel sur love4numscreen le tirage
-  interstitialId: "ca-app-pub-6996110830232375/1579378159",
-  //creditBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - la banner large sur creditscreen
-  creditBannerId: "ca-app-pub-6996110830232375/9976742927",
+  // *
+  // *
+  // *
+
+  // TODO POUR CTIVER LA VERSION DE PRODUCTION
+  // ! ID's DE PRODUCTION
+
+  // infoBannerId: "ca-app-pub-6996110830232375/1483175230", // PROD - les 2 banner de infoscreen
+  // loveBannerId: "ca-app-pub-6996110830232375/9643987853", // PROD - la banner sur love4numscreen le tirage
+  // interstitialId: "ca-app-pub-6996110830232375/1579378159", // PROD l'intersticiel sur love4numscreen le tirage
+  // creditBannerId: "ca-app-pub-6996110830232375/9976742927", // PROD - la banner large sur creditscreen
+
+  // *
+  // *
+  // *
+  // *
+
+  // TODO POUR CTIVER LA VERSION DE TEST
+  // ! ID's DE TEST
+
+  infoBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - les 2 banner de infoscreen
+  loveBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - la banner sur love4numscreen le tirage
+  interstitialId: "ca-app-pub-3940256099942544/4411468910", // TEST l'intersticiel sur love4numscreen le tirage
+  creditBannerId: "ca-app-pub-3940256099942544/9214589741", // TEST - la banner large sur creditscreen
 };
